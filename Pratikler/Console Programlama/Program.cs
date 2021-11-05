ef33e4afa13f59dacf4c8a4957647fd7979eaecd
Veysel Emre Yemlik <emreyemlik1905@gmail.com>
2021-11-05 13:03:24 +0300

    Add files via upload

---
+++ Pratikler/Console Programlama/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Console_Programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi giriniz:");
+            string name = Console.WriteLine();
+            Console.WriteLine("Soisminizi giriniz:");
+            string surname = Console.WriteLine();
+            Console.WriteLine("Merhaba" + name + " "+ surname);
+
+        }
+    }
+}
